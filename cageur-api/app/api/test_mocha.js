3ed684c74dab427808f1698e7608f404ff6963fd
Guta Saputra <gutasaputra@gmail.com>
2017-01-07 07:11:05 +0700

    update test mocha. fixing POST and UPDATE, with dummy data id.

--- cageur-api/app/api/test_mocha.js
+++ cageur-api/app/api/test_mocha.js
@@ -1,3 +1,6 @@
+let express = require('express');
+let app = express();
+
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('server');
@@ -7,9 +10,48 @@ let should = require("should");
 let request = require("superagent");
 let util = require("util");
 
+let id_dummy = 6;
 chai.use(chaiHttp);
 
 describe('API Clinic Test', function() {
+
+  it('should add a SINGLE clinic on /api/v1/clinic POST', function(done) {
+  var data = {
+    clinic_name: 'Clinic Dummy New',
+    address: 'Bandung',
+    phone: '8',
+    fax: '9',
+  };
+
+  chai.request('http://localhost:5000')
+    .post('/api/v1/clinic/')
+    .send(data)
+    .end(function(err, res){
+        expect(res.status).to.equal(200);
+        console.log(err)
+        done();
+    });
+  });
+
+
+  it('should update a SINGLE clinic on /api/v1/clinic/<id> PUT', function(done) {
+  var data = {
+    clinic_name: 'Clinic Dummy Update 2',
+    address: 'Bandung Update',
+    phone: '9',
+    fax: '8',
+  };
+
+  chai.request('http://localhost:5000')
+    .put('/api/v1/clinic/' + id_dummy)
+    .send(data)
+    .end(function(err, res){
+        console.log(err)
+        expect(res.status).to.equal(200);
+        done();
+    });
+  });
+
   it('should list ALL clinic on /api/v1/clinic GET', function(done) {
   chai.request('http://localhost:5000')
     .get('/api/v1/clinic')
@@ -21,72 +63,26 @@ describe('API Clinic Test', function() {
 
   it('should list a SINGLE clinic on /api/v1/clinic/<id> GET', function(done) {
   chai.request('http://localhost:5000')
-    .get('/api/v1/clinic/1')
+    .get('/api/v1/clinic/' + id_dummy)
     .end(function(err, res){
-       expect(res.status).to.equal(500);
+       expect(res.status).to.equal(200);
       done();
     });
   });
-
-  it('should add a SINGLE clinic on /api/v1/clinic POST', function(done) {
-  chai.request('http://localhost:5000')
-      .post('/api/v1/clinic')
-      .send({'clinic_name': 'Clinic Dummy', 'address': 'Bandung', 'phone':'888 888', 'fax':'888 888'})
-      .end(function(err, res){
-        expect(res.status).to.equal(500);
-        // res.should.be.json;
-        // res.body.should.be.a('object');
-        // res.body.should.have.property('SUCCESS');
-        // res.body.SUCCESS.should.be.a('object');
-        // res.body.SUCCESS.should.have.property('clinic_name');
-        // res.body.SUCCESS.should.have.property('address');
-        // res.body.SUCCESS.should.have.property('_id');
-        // res.body.SUCCESS.clinic_name.should.equal('Clinic Dummy');
-        // res.body.SUCCESS.address.should.equal('Bandung');
-        done();
-      });
-  });
-
-  it('should update a SINGLE clinic on /api/v1/clinic/<id> PUT', function(done) {
-  chai.request('http://localhost:5000')
-    .get('/api/v1/clinic')
-    .end(function(err, res){
-      chai.request('http://localhost:5000')
-        .put('/api/v1/clinic/1')
-        .send({'clinic_name': 'Clinic Dummy Update', 'address': 'Bandung', 'phone':'888 888', 'fax':'888 888'})
-        .end(function(error, response){
-          expect(response.status).to.equal(500);
-          // response.should.be.json;
-          // response.body.should.be.a('object');
-          // response.body.should.have.property('UPDATED');
-          // response.body.UPDATED.should.be.a('object');
-          // response.body.UPDATED.should.have.property('clinic_name');
-          // response.body.UPDATED.clinic_name.should.equal('Clinic Dummy Update');
-          done();
-      });
-    });
-  });
-
 
   it('should delete a SINGLE clinic on /api/v1/clinic/<id> DELETE', function(done) {
   chai.request('http://localhost:5000')
     .get('/api/v1/clinic')
     .end(function(err, res){
         chai.request('http://localhost:5000')
-          .delete('/api/v1/clinic/1')
+          .delete('/api/v1/clinic/' + id_dummy)
           .end(function(error, response){
             expect(response.status).to.equal(200);
-            // response.should.be.json;
-            // response.body.should.be.a('object');
-            // response.body.should.have.property('REMOVED');
-            // response.body.REMOVED.should.be.a('object');
-            // response.body.REMOVED.should.have.property('name');
-            // response.body.REMOVED.should.have.property('_id');
-            // response.body.REMOVED.name.should.equal('Bat');
             done();
         });
       });
   });
+
 
 });
 

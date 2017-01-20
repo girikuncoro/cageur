4ce154cd4f49a5453abcdc9626372876ac07ed1a
Rudy Prihantoro <prihantoro.rudy@gmail.com>
2017-01-20 07:02:16 +0700

    translate to bahasa and refactor unnecessary component out from Mail route

--- cageur-ui/src/routes/Mail.js
+++ cageur-ui/src/routes/Mail.js
@@ -22,11 +22,11 @@ import {
 @withRouter
 export default class Mail extends React.Component {
   handleClick(e) {
-    this.props.router.push('/ltr/mailbox/compose');
+    this.props.router.push('/mailbox/compose');
   }
 
   handleBackClick(e) {
-    this.props.router.push('/ltr/mailbox/inbox');
+    this.props.router.push('/mailbox/inbox');
   }
 
   handleTextareaClick(e) {
@@ -43,7 +43,7 @@ export default class Mail extends React.Component {
 
   render() {
     return (
-      <PanelContainer className='inbox'>
+      <PanelContainer className='inbox' controls={false}>
         <Panel>
           <PanelBody style={{paddingTop: 0}}>
             <Grid>
@@ -51,24 +51,9 @@ export default class Mail extends React.Component {
                 <Col xs={8} style={{paddingTop: 12.5}}>
                   <ButtonToolbar className='inbox-toolbar'>
                     <ButtonGroup>
-                      <Button outlined onlyOnHover bsStyle='darkgreen45' onClick={::this.handleBackClick}><Icon glyph='icon-dripicons-return'/></Button>
-                    </ButtonGroup>
-                    <ButtonGroup>
                       <Button bsStyle='blue' onClick={::this.handleClick}>
                         <Icon glyph='icon-fontello-edit-1'/>
                       </Button>
-                    </ButtonGroup>
-                    <ButtonGroup className='hidden-xs'>
-                      <Button outlined onlyOnHover bsStyle='darkgreen45'><Icon glyph='icon-fontello-reply'/></Button>
-                      <Button outlined onlyOnHover bsStyle='darkgreen45'><Icon glyph='icon-fontello-reply-all-1'/></Button>
-                      <Button outlined onlyOnHover bsStyle='darkgreen45'><Icon glyph='icon-fontello-forward'/></Button>
-                    </ButtonGroup>
-                    <ButtonGroup className='hidden-xs'>
-                      <Button outlined onlyOnHover bsStyle='danger' className='text-center'><Icon glyph='icon-fontello-attention-alt'/></Button>
-                      <Button outlined onlyOnHover bsStyle='danger'><Icon glyph='icon-fontello-trash-1'/></Button>
-                    </ButtonGroup>
-                    <ButtonGroup className='hidden-xs hidden-sm'>
-                      <Button outlined onlyOnHover bsStyle='brown' className='text-center' onClick={::this.print}><Icon glyph='icon-outlined-printer'/></Button>
                     </ButtonGroup>
                   </ButtonToolbar>
                 </Col>
@@ -92,13 +77,13 @@ export default class Mail extends React.Component {
                       <div className='inbox-avatar'>
                         <img src='/imgs/app/avatars/avatar5.png' width='40' height='40' className='border-green hidden-xs' />
                         <div className='inbox-avatar-name'>
-                          <div className='fg-darkgrayishblue75'><strong>From: </strong>Jordyn Ouellet - <em>jordyn_ouellet@example.com</em></div>
-                          <div className='fg-darkgray40'><strong>Subject: </strong>Regd financial projections for the next five years</div>
+                          <div className='fg-darkgrayishblue75'><strong>Kepada: </strong>Jordyn Ouellet - <em>jordyn_ouellet@example.com</em></div>
+                          <div className='fg-darkgray40'><strong>Judul: </strong>Regd financial projections for the next five years</div>
                         </div>
                         <div className='inbox-date fg-darkgray40 text-right hidden-xs'>
                           <div style={{position: 'relative', top: 5}}>
                             <Badge className='bg-blue fg-white'>
-                              OPPORTUNITIES
+                              DIABETES
                             </Badge>
                           </div>
                           <div style={{position: 'relative'}}><small>
@@ -143,35 +128,6 @@ export default class Mail extends React.Component {
                   </Row>
                 </Grid>
                 <hr/>
-                <Grid className='hidden-print'>
-                  <Row>
-                    <Col xs={12}>
-                      <p>
-                        <strong>Attachments</strong><span className='fg-darkgray40'> (3 files, 680 KB)</span>
-                      </p>
-                      <p className='inbox-attachments'>
-                        <Icon glyph='icon-1-and-quarter-x icon-outlined-image' /><span><strong> force.gif</strong> <span className='fg-darkgray40'>(128 KB)</span></span>
-                        <a className='inbox-attachment-download' href='#'>Download</a>
-                      </p>
-                      <p className='inbox-attachments'>
-                        <Icon glyph='icon-1-and-quarter-x icon-outlined-image' /><span><strong> lightsaber.png</strong> <span className='fg-darkgray40'>(450 KB)</span></span>
-                        <a className='inbox-attachment-download' href='#'>Download</a>
-                      </p>
-                      <p className='inbox-attachments' style={{marginBottom: 0}}>
-                        <Icon glyph='icon-1-and-quarter-x devicon-html5-plain-wordmark' /><span><strong> hax.html</strong> <span className='fg-darkgray40'>(2 KB)</span></span>
-                        <a className='inbox-attachment-download' href='#'>Download</a>
-                      </p>
-                    </Col>
-                  </Row>
-                </Grid>
-                <hr className='hidden-print'/>
-                <Grid className='hidden-print'>
-                  <Row>
-                    <Col xs={12}>
-                      <FormControl componentClass="textarea" rows='5' onClick={::this.handleTextareaClick} onFocus={::this.handleTextareaClick} placeholder="Click here to reply" style={{marginBottom: 25}} />
-                    </Col>
-                  </Row>
-                </Grid>
               </PanelBody>
             </Panel>
           </PanelBody>

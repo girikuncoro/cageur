6305e69fbdf5134d44bea401a267953ff3b7f5d2
Giri Kuncoro <girikuncoro@gmail.com>
2017-01-22 11:43:15 -0800

    Add procfile and build prod for UI

--- cageur-ui/src/common/constant.js
+++ cageur-ui/src/common/constant.js
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:5000/api/v1';
+export const API_URL = process.env.CAGEUR_API_URL || 'http://localhost:5000/api/v1';
 export const API_HEADERS = {
   'Content-Type': 'application/json'
 }

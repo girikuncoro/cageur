7cd613559cb7d832daa216ec036d733cdac7ad45
Giri Kuncoro <girikuncoro@gmail.com>
2017-02-12 13:35:53 -0800

    Test main function for user module

--- cageur-api/tests/api/user/test-user.js
+++ cageur-api/tests/api/user/test-user.js
@@ -68,66 +68,133 @@ describe('User API Test', () => {
     });
   });
 
-  // describe('POST /api/v1/user', () => {
-  //   const countRow = (table) => {
-  //     return db.one(`SELECT COUNT(*) FROM ${table}`);
-  //   };
-  //
-  //   afterEach((done) => {
-  //     db.none(`DELETE FROM clinic WHERE id=${cleanedID}`)
-  //     .then(_ => done());
-  //   });
-  //
-  //   it('should insert new clinic', (done) => {
-  //     const validRequest = {
-  //       name: 'klinik persija',
-  //       address: 'jalan jakmania',
-  //       phone_number: '333',
-  //     };
-  //
-  //     chai.request(app)
-  //     .post('/api/v1/clinic')
-  //     .send(validRequest)
-  //     .then((res) => {
-  //       const r = res.body;
-  //
-  //       expect(res.status).to.equal(200);
-  //       expect(r.status).to.equal('success');
-  //
-  //       expect(r.data.name).to.equal('klinik persija');
-  //       expect(r.data.address).to.equal('jalan jakmania');
-  //       expect(r.data['phone_number']).to.equal('333');
-  //
-  //       cleanedID = r.data.id;
-  //
-  //       countRow('clinic').then((row) => {
-  //         expect(parseInt(row.count, 10)).to.equal(3);
-  //         done();
-  //       });
-  //     });
-  //   });
-  //
-  //   it('should return 400 for missing parameters', (done) => {
-  //     const invalidRequests = [
-  //       {},
-  //       { address: 'foo bar' },
-  //       { phone_number: '999' },
-  //     ];
-  //
-  //     invalidRequests.forEach((req, i) => {
-  //       chai.request(app)
-  //       .post('/api/v1/clinic')
-  //       .send(req)
-  //       .then((_) => {}, (err) => {
-  //         const data = err.response.body;
-  //
-  //         expect(err.status).to.equal(400);
-  //         expect(data.status).to.equal('error');
-  //         expect(data.message).to.equal('Missing required parameters "name"');
-  //
-  //         if (i === invalidRequests.length - 1) done();
-  //       });
-  //     });
-  //   });
-  // });
+  describe('POST /api/v1/user', () => {
+    const countRow = (table) => {
+      return db.one(`SELECT COUNT(*) FROM ${table}`);
+    };
+
+    it('should insert new user with encrypted password', (done) => {
+      const validRequest = {
+        name: 'Caca',
+        email: 'caca@foo.com',
+        password: 'Cageur@123!',
+        role: 'superadmin',
+      };
+
+      chai.request(app)
+      .post('/api/v1/user')
+      .send(validRequest)
+      .then((res) => {
+        const r = res.body;
+
+        expect(res.status).to.equal(200);
+        expect(r.status).to.equal('success');
+
+        expect(r.data.name).to.equal('Caca');
+        expect(r.data.email).to.equal('caca@foo.com');
+        expect(r.data.role).to.equal('superadmin');
+
+        countRow('cageur_user').then((row) => {
+          expect(parseInt(row.count, 10)).to.equal(3);
+        });
+
+        // password should be hashed
+        return db.one(`SELECT password FROM cageur_user WHERE id = ${r.data.id}`);
+      })
+      .then((user) => {
+        expect(user.password).to.not.equal('Cageur@123!');
+        done();
+      });
+    });
+
+    it('should return 400 for missing parameters', (done) => {
+      const invalidRequests = [
+        {},
+        { email: 'bla@foo.com' },
+        { password: 'Cageur@123!' },
+        { role: 'clinic' },
+      ];
+
+      invalidRequests.forEach((req, i) => {
+        chai.request(app)
+        .post('/api/v1/user')
+        .send(req)
+        .then((_) => {}, (err) => {
+          const data = err.response.body;
+
+          expect(err.status).to.equal(400);
+          expect(data.status).to.equal('error');
+          expect(data.message).to.equal('Missing required parameters "name" or "email" or "password" or "role"');
+
+          if (i === invalidRequests.length - 1) done();
+        });
+      });
+    });
+
+    it('should return 400 for invalid email format', (done) => {
+      const invalidRequest = {
+        name: 'Dodo',
+        email: 'invalid email',
+        password: 'Cageur@123!',
+        role: 'superadmin',
+      };
+
+      chai.request(app)
+      .post('/api/v1/user')
+      .send(invalidRequest)
+      .then((_) => {}, (err) => {
+        const data = err.response.body;
+
+        expect(err.status).to.equal(400);
+        expect(data.status).to.equal('error');
+        expect(data.message).to.equal('Invalid email format');
+
+        done();
+      });
+    });
+
+    it('should return 400 for invalid password format', (done) => {
+      const invalidRequest = {
+        name: 'Dodo',
+        email: 'dodo@foo.com',
+        password: 'invalid password',
+        role: 'superadmin',
+      };
+
+      chai.request(app)
+      .post('/api/v1/user')
+      .send(invalidRequest)
+      .then((_) => {}, (err) => {
+        const data = err.response.body;
+
+        expect(err.status).to.equal(400);
+        expect(data.status).to.equal('error');
+        expect(data.message).to.equal('Invalid password requirement, must contain at least 1 special character, uppercase, alphabet, number and minimum 8 characters');
+
+        done();
+      });
+    });
+
+    it('should return 400 for duplicated email', (done) => {
+      const invalidRequest = {
+        name: 'Ali',
+        email: 'ali@foo.com',
+        password: 'Cageur@123!',
+        role: 'superadmin',
+      };
+
+      chai.request(app)
+      .post('/api/v1/user')
+      .send(invalidRequest)
+      .then((_) => {}, (err) => {
+        const data = err.response.body;
+
+        expect(err.status).to.equal(400);
+        expect(data.status).to.equal('error');
+        expect(data.message).to.equal('Email address exist');
+
+        done();
+      });
+    });
+  });
 });

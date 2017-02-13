2dc310a16a7c03bbd6d84451a15ee8c41bfccc65
Rudy Prihantoro <prihantoro.rudy@gmail.com>
2017-02-13 23:14:45 +0700

    remove datatable and install react-bootstrap-table

--- simkuring-ui/src/plugins.js
+++ simkuring-ui/src/plugins.js
@@ -4,4 +4,4 @@ import 'js/modernizr.js';
 import 'js/perfect-scrollbar.js';
 import 'js/jquery.js';
 
-import "bower_components/datatables/media/js/jquery.dataTables.js";
+import "bower_components/bootstrap/dist/js/bootstrap.js";

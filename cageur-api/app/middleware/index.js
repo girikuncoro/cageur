909aea3ac74621068484108d1952c4d4d09aafaa
Giri Kuncoro <girikuncoro@gmail.com>
2017-02-14 22:51:53 -0800

    Add clinic level permission

--- cageur-api/app/middleware/index.js
+++ cageur-api/app/middleware/index.js
@@ -1,5 +1,19 @@
 const abort = require('../util/abort');
 
+const extractClinicID = (req) => {
+  // example: /api/v1/message/schedule/clinic/1
+  const originalUrl = req.originalUrl;
+  // example: /api/v1/message/schedule
+  const baseUrl = req.baseUrl;
+  const params = originalUrl.split(baseUrl)[1];
+
+  if (params.indexOf('/clinic/') > -1) {
+    const clinicID = params.split('/clinic/')[1];
+    return parseInt(clinicID, 10);
+  }
+  return null;
+};
+
 module.exports = {
   authenticate(passport) {
     return passport.authenticate('jwt', { session: false });
@@ -7,12 +21,21 @@ module.exports = {
 
   isAuthorized: {
     clinic(req, res, next) {
-      console.log('auth clinic: ', JSON.stringify(req.user));
-      if (req.user.role === 'clinic') {
+      // superadmin permission can do anything
+      if (req.user.role === 'superadmin') {
         return next();
       }
-      return next(abort(401, 'Must have clinic level permission', JSON.stringify(req.user)));
+      // for clinic permission, must check if requested its own clinic data
+      if (req.user.role === 'clinic') {
+        const clinicID = extractClinicID(req);
+        if (clinicID && req.user['clinic_id'] !== clinicID) {
+          return next(abort(401, 'Must have clinic permission', JSON.stringify(req.user)));
+        }
+        return next();
+      }
+      return next(abort(401, 'Must have permission', JSON.stringify(req.user)));
     },
+
     superAdmin(req, res, next) {
       if (req.user.role === 'superadmin') {
         return next();

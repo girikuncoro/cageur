54de7339d151fc68eea1c3d39d616da90b942c60
Giri Kuncoro <girikuncoro@gmail.com>
2017-02-16 14:18:54 -0800

    Add pdg id to clinic endpoint

--- cageur-api/app/api/patient-disease-group/controller.js
+++ cageur-api/app/api/patient-disease-group/controller.js
@@ -65,7 +65,7 @@ const ctl = {
     const sqlGetAllDataWithClinicID = `
       SELECT json_build_object(
           'patient', row_to_json(p.*),
-          'disease_group', array_remove(array_agg(DISTINCT dg.*), NULL)) AS patient_disease_group
+          'disease_group', COALESCE(array_agg(json_build_object('pdg_id', pdg.id, 'disease', dg.*)) FILTER (WHERE pdg.id IS NOT NULL))) AS patient_disease_group
       FROM patient p
       LEFT OUTER JOIN patient_disease_group pdg
         ON pdg.patient_id = p.id

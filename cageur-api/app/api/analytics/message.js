d45c74697cdbb25f088e9a9b9a4a57612403e452
Giri Kuncoro <girikuncoro@gmail.com>
2017-02-16 22:18:40 -0800

    Protect analytics message API

--- cageur-api/app/api/analytics/message.js
+++ cageur-api/app/api/analytics/message.js
@@ -1,16 +1,17 @@
 const router = require('express').Router();
 const ctl = require('../analytics/message-controller');
+const { isAuthorized } = require('../../middleware');
 
 /**
 * Retrieve all message analytics data
 * GET /api/v1/analytics/message
 */
-router.get('/', ctl.getAllMessage);
+router.get('/', isAuthorized.superAdmin, ctl.getAllMessage);
 
 /**
 * Retrieve all message analytics data from clinicID
 * GET /api/v1/analytics/message/clinic/:id
 */
-router.get('/clinic/:id', ctl.getAllMessageWithClinicID);
+router.get('/clinic/:id', isAuthorized.clinicSelf, ctl.getAllMessageWithClinicID);
 
 module.exports = router;

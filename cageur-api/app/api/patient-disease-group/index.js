7203a89d0e001e4374d376fde2bcdcd07e3bb718
Giri Kuncoro <girikuncoro@gmail.com>
2017-02-16 22:16:55 -0800

    Protect patient disease group API

--- cageur-api/app/api/patient-disease-group/index.js
+++ cageur-api/app/api/patient-disease-group/index.js
@@ -1,46 +1,47 @@
 const router = require('express').Router();
 const ctl = require('../patient-disease-group/controller');
+const { isAuthorized } = require('../../middleware');
 
 /**
 * Insert patient disease group data
 * POST /api/v1/patient_disease_group
 */
-router.post('/', ctl.createPatientDiseaseGroup);
+router.post('/', isAuthorized.superAdmin, ctl.createPatientDiseaseGroup);
 
 /**
 * Retrieve all patient disease group data
 * GET /api/v1/patient_disease_group
 */
-router.get('/', ctl.getAllPatientDiseaseGroup);
+router.get('/', isAuthorized.superAdmin, ctl.getAllPatientDiseaseGroup);
 
 /**
 * Retrieve all patient disease group data with clinic id
 * GET /api/v1/patient_disease_group/clinic/:id
 */
-router.get('/clinic/:id', ctl.getAllPatientDiseaseGroupWithClinicID);
+router.get('/clinic/:id', isAuthorized.clinicSelf, ctl.getAllPatientDiseaseGroupWithClinicID);
 
 /**
 * Retrieve single patient disease group data
 * GET /api/v1/patient_disease_group/:id
 */
-router.get('/:id', ctl.getSinglePatientDiseaseGroup);
+router.get('/:id', isAuthorized.superAdmin, ctl.getSinglePatientDiseaseGroup);
 
 /**
 * Retrieve single patient disease group data with patient id
 * GET /api/v1/patient_disease_group/patient/:id
 */
-router.get('/patient/:id', ctl.getSinglePatientDiseaseGroupWithPatientID);
+router.get('/patient/:id', isAuthorized.superAdmin, ctl.getSinglePatientDiseaseGroupWithPatientID);
 
 /**
 * Update patient disease group data
 * PUT /api/v1/patient_disease_group/:id
 */
-router.put('/:id', ctl.updatePatientDiseaseGroup);
+router.put('/:id', isAuthorized.superAdmin, ctl.updatePatientDiseaseGroup);
 
 /**
 * Remove patient disease group
 * DELETE /api/v1/patient_disease_group/:id
 */
-router.delete('/:id', ctl.removePatientDiseaseGroup);
+router.delete('/:id', isAuthorized.superAdmin, ctl.removePatientDiseaseGroup);
 
 module.exports = router;

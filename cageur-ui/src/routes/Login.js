eefc57e2a11494e6b4515216e9a8bb329f643f39
Rudy Prihantoro <prihantoro.rudy@gmail.com>
2017-02-26 22:17:25 +0700

    initial functional login

--- cageur-ui/src/routes/Login.js
+++ cageur-ui/src/routes/Login.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classNames from 'classnames';
 import { Link, withRouter } from 'react-router';
+import {API_URL} from '../common/constant';
 
 import {
   Row,
@@ -22,24 +23,68 @@ import {
 } from '@sketchpixy/rubix';
 
 @withRouter
-export default class Login extends React.Component {
-  back(e) {
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
+  handleEmailInput(e) {
+    this.setState({ email: e.target.value });
+  }
+
+  handlePasswordInput(e) {
+    this.setState({ password: e.target.value });
+  }
+
+  handleLogin(e) {
     e.preventDefault();
     e.stopPropagation();
-    this.props.router.goBack();
-  }
+    const creds = {
+      email: this.state.email,
+      password: this.state.password
+    }
+    const endpoint = '/auth';
+    const body = {
+      email: creds.email,
+      password: creds.password
+    }
 
-  componentDidMount() {
-    $('html').addClass('authentication');
-  }
+    fetch(`${API_URL}${endpoint}`, {
+      method: 'POST',
+      headers: { 'Content-Type':'application/json' },
+      body: JSON.stringify(body)
+    })
+    .then(function(response) {
+      if (response.ok) {
+        return response.json()
+      } else {
+        // Redirect to login page when response not ok
+        this.props.router.push("/login");
 
-  componentWillUnmount() {
-    $('html').removeClass('authentication');
-  }
+        localStorage.setItem('token', '');
+      }
+    })
+    .then((responseData) => {
+      console.log(responseData);
 
-  getPath(path) {
-    path = `/${path}`;
-    return path;
+      // If login was successful, set the token in local storage
+      localStorage.setItem('token', responseData.token);
+
+      // Redirect to dashboard
+      this.props.router.push("/dashboard");
+    })
+    .catch((error) => {
+      console.log('Error fetching and parsing data', error);
+      this.setState({
+        email: '',
+        password: ''
+      });
+    })
   }
 
   render() {
@@ -54,32 +99,24 @@ export default class Login extends React.Component {
                     <Panel>
                       <PanelBody style={{padding: 0}}>
                         <div className='text-center bg-darkblue fg-white'>
-                          <h3 style={{margin: 0, padding: 25}}>Sign in to Rubix</h3>
+                          <h3 style={{margin: 0, padding: 25}}>Masuk ke Cageur</h3>
                         </div>
                         <div className='bg-hoverblue fg-black50 text-center' style={{padding: 12.5}}>
-                          <div>You need to sign in for those awesome features</div>
-                          <div style={{marginTop: 12.5, marginBottom: 12.5}}>
-                            <Button id='facebook-btn' lg bsStyle='darkblue' type='submit' onClick={::this.back}>
-                              <Icon glyph='icon-fontello-facebook' />
-                              <span>Sign in <span className='hidden-xs'>with facebook</span></span>
-                            </Button>
-                          </div>
-                          <div>
-                            <a id='twitter-link' href='#' onClick={::this.back}><Icon glyph='icon-fontello-twitter' /><span> or with twitter</span></a>
-                          </div>
-                        </div>
-                        <div>
-                          <div className='text-center' style={{padding: 12.5}}>
-                            or use your Rubix account
-                          </div>
+                          <div>Anda perlu masuk ke Cageur terlebih dahulu</div>
                           <div style={{padding: 25, paddingTop: 0, paddingBottom: 0, margin: 'auto', marginBottom: 25, marginTop: 25}}>
-                            <Form onSubmit={::this.back}>
+                            <Form onSubmit={::this.handleLogin}>
                               <FormGroup controlId='emailaddress'>
                                 <InputGroup bsSize='large'>
                                   <InputGroup.Addon>
                                     <Icon glyph='icon-fontello-mail' />
                                   </InputGroup.Addon>
-                                  <FormControl autoFocus type='email' className='border-focus-blue' placeholder='support@sketchpixy.com' />
+                                  <FormControl autoFocus
+                                                type='email'
+                                                className='border-focus-blue'
+                                                placeholder='cageur@email.com'
+                                                value={this.state.email}
+                                                onChange={::this.handleEmailInput}
+                                                />
                                 </InputGroup>
                               </FormGroup>
                               <FormGroup controlId='password'>
@@ -87,17 +124,19 @@ export default class Login extends React.Component {
                                   <InputGroup.Addon>
                                     <Icon glyph='icon-fontello-key' />
                                   </InputGroup.Addon>
-                                  <FormControl type='password' className='border-focus-blue' placeholder='password' />
+                                  <FormControl type='password'
+                                              className='border-focus-blue'
+                                              placeholder='sandi'
+                                              value={this.state.password}
+                                              onChange={::this.handlePasswordInput}
+                                              />
                                 </InputGroup>
                               </FormGroup>
                               <FormGroup>
                                 <Grid>
                                   <Row>
-                                    <Col xs={6} collapseLeft collapseRight style={{paddingTop: 10}}>
-                                      <Link to={::this.getPath('signup')}>Create a Rubix account</Link>
-                                    </Col>
                                     <Col xs={6} collapseLeft collapseRight className='text-right'>
-                                      <Button outlined lg type='submit' bsStyle='blue' onClick={::this.back}>Login</Button>
+                                      <Button outlined lg type='submit' bsStyle='blue' onClick={::this.handleLogin}>Masuk</Button>
                                     </Col>
                                   </Row>
                                 </Grid>

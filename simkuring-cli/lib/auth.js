296fea32c1cddad920a1b1bcde5bcf56644465b2
Rudy Prihantoro <prihantoro.rudy@gmail.com>
2017-04-01 23:45:36 +0700

    refactor auth

---
+++ simkuring-cli/lib/auth.js
@@ -0,0 +1,71 @@
+var chalk       = require('chalk');
+var program     = require('commander');
+var co          = require('co');
+var prompt      = require('co-prompt');
+var request     = require('superagent');
+var CLI         = require('clui');
+var Spinner     = CLI.Spinner;
+var Preferences = require('preferences');
+
+function getCageurCredentials(callback) {
+  program
+    .arguments('<file>')
+    .option('-u, --email <email>', 'The user email to authenticate as')
+    .option('-p, --password <password>', 'The user\'s password')
+    .action(function(file) {
+      co(function *() {
+        var email = yield prompt('email: ');
+        var password = yield prompt.password('password: ');
+        callback(email, password);
+      });
+    })
+    .parse(process.argv);
+}
+
+function getCageurToken(callback) {
+  var prefs = new Preferences('cageur');
+
+  if (prefs.cageur && prefs.cageur.token) {
+    console.log(chalk.green('Already signed in.'));
+    return callback(null, prefs.cageur.token);
+  }
+
+  getCageurCredentials(function(email, password) {
+    var status = new Spinner('Authenticating you, please wait...');
+    status.start();
+    request
+       .post('http://localhost:5000/api/v1/auth')
+       .set('Accept', 'application/json')
+       .send({
+         "email": email,
+         "password": password
+       })
+       .end(function (err, res) {
+          if (!err && res.ok) {
+            status.stop();
+            var token = JSON.parse(res.text).token;
+            console.log(token);
+            prefs.cageur = {
+              token : token
+            };
+            return callback(null, token);
+          }
+
+          if (err) {
+            status.stop();
+            return callback(err);
+          }
+        });
+  })
+}
+
+module.exports = {
+  cageurAuth: function cageurAuth(callback) {
+    getCageurToken(function(err, token) {
+      if (err) {
+        return callback(err);
+      }
+      return callback(null, token);
+    });
+  }
+}

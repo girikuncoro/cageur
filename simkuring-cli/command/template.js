2c8170b48e3b0bd1a98cce3321f6d68a40c18d91
Giri Kuncoro <gkuncoro@vmware.com>
2017-04-16 18:45:39 -0700

    Add get template cli

---
+++ simkuring-cli/command/template.js
@@ -0,0 +1,41 @@
+const { Command, Action } = require('../command/base');
+const CageurClient = require('../client');
+const config = require('../config');
+const { print } = require('../utils');
+
+class TemplateAction extends Action {
+  constructor(client, config) {
+    super(client, config, { url: '/template' });
+  }
+
+  go(cmd) {
+    if (!this.client.isValid()) {
+      print.warning('Token or API target not set');
+      return process.exit();
+    }
+    if (cmd === 'get') {
+      super.get();
+    }
+  }
+}
+
+class TemplateCommand {
+  static factory(Program, Action) {
+    const client = new CageurClient({
+      targetUrl: config.url,
+      token: config.token,
+    });
+    
+    const action = new Action(client, config);
+
+    const cmd = new Command(Program, {
+      object: 'template <cmd>',
+      description: 'High quality reminder template from doctors',
+      action: (cmd) => action.go(cmd),
+    });
+
+    return cmd.execute();
+  }
+}
+
+module.exports = { TemplateAction, TemplateCommand };
--- simkuring-cli/index.js
+++ simkuring-cli/index.js
@@ -4,6 +4,7 @@ const { AboutCommand, AboutAction } = require('./command/about');
 const { ClinicCommand, ClinicAction } = require('./command/clinic');
 const { PatientCommand, PatientAction } = require('./command/patient');
 const { TargetCommand, TargetAction } = require('./command/target');
+const { TemplateCommand, TemplateAction } = require('./command/template');
 
 const Spinner = require('clui').Spinner;
 const loading = new Spinner('Loading Simkuring CLI action ...');
@@ -14,6 +15,7 @@ AboutCommand.factory(Program, AboutAction);
 ClinicCommand.factory(Program, ClinicAction);
 PatientCommand.factory(Program, PatientAction);
 TargetCommand.factory(Program, TargetAction);
+TemplateCommand.factory(Program, TemplateAction);
 
 Program.parse(process.argv);
 loading.stop();

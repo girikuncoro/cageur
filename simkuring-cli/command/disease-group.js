142730a138209977a16a61e93b01602dee013132
Giri Kuncoro <gkuncoro@vmware.com>
2017-04-16 18:50:07 -0700

    Add get for disease group cli

---
+++ simkuring-cli/command/disease-group.js
@@ -0,0 +1,41 @@
+const { Command, Action } = require('../command/base');
+const CageurClient = require('../client');
+const config = require('../config');
+const { print } = require('../utils');
+
+class DiseaseGroupAction extends Action {
+  constructor(client, config) {
+    super(client, config, { url: '/disease_group' });
+  }
+
+  go(cmd) {
+    if (!this.client.isValid()) {
+      print.warning('Token or API target not set');
+      return process.exit();
+    }
+    if (cmd === 'get') {
+      super.get();
+    }
+  }
+}
+
+class DiseaseGroupCommand {
+  static factory(Program, Action) {
+    const client = new CageurClient({
+      targetUrl: config.url,
+      token: config.token,
+    });
+    
+    const action = new Action(client, config);
+
+    const cmd = new Command(Program, {
+      object: 'disease-group <cmd>',
+      description: 'Disease group information',
+      action: (cmd) => action.go(cmd),
+    });
+
+    return cmd.execute();
+  }
+}
+
+module.exports = { DiseaseGroupAction, DiseaseGroupCommand };
--- simkuring-cli/index.js
+++ simkuring-cli/index.js
@@ -2,6 +2,7 @@
 const Program = require('commander');
 const { AboutCommand, AboutAction } = require('./command/about');
 const { ClinicCommand, ClinicAction } = require('./command/clinic');
+const { DiseaseGroupCommand, DiseaseGroupAction } = require('./command/disease-group');
 const { PatientCommand, PatientAction } = require('./command/patient');
 const { TargetCommand, TargetAction } = require('./command/target');
 const { TemplateCommand, TemplateAction } = require('./command/template');
@@ -13,6 +14,7 @@ loading.start();
 // List of command factory
 AboutCommand.factory(Program, AboutAction);
 ClinicCommand.factory(Program, ClinicAction);
+DiseaseGroupCommand.factory(Program, DiseaseGroupAction);
 PatientCommand.factory(Program, PatientAction);
 TargetCommand.factory(Program, TargetAction);
 TemplateCommand.factory(Program, TemplateAction);

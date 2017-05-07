ff7f576a60628555b350e6519f46b6d2f9962b0b
Rudy Prihantoro <prihantoro.rudy@gmail.com>
2017-05-07 19:51:20 +0700

    implement csv import for disease group and add csv file example

--- simkuring-cli/command/disease-group.js
+++ simkuring-cli/command/disease-group.js
@@ -1,16 +1,23 @@
 const { Command, Action } = require('../command/base');
 const CageurClient = require('../client');
 const config = require('../config');
-const { print } = require('../utils');
+const { print, file } = require('../utils');
 
 class DiseaseGroupAction extends Action {
   constructor(client, config) {
     super(client, config, { url: '/disease_group' });
   }
 
-  go(cmd) {
+  go(cmd, option) {
     if (cmd === 'get') {
       super.get();
+    }
+    if (cmd === 'import') {
+      if (!option.inputfile) {
+        print.warning('Please specify csv/xlsx file for import and make sure it exists');
+        return process.exit();
+      }
+      super.import(option.inputfile);
     }
   }
 }
@@ -27,9 +34,9 @@ class DiseaseGroupCommand {
     const cmd = new Command(Program, {
       object: 'disease-group <cmd>',
       description: 'Disease group information',
-      action: (cmd) => action.go(cmd),
+      action: (cmd, option) => action.go(cmd, option),
     });
-
+    cmd.addOption('-f, --inputfile <inputfile>', 'CSV/XLSX file to import and bulk insert patient info');
     return cmd.execute();
   }
 }

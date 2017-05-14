b73a1c640323848e82a7593eeb8b3b5c1030d4f1
Rudy Prihantoro <prihantoro.rudy@gmail.com>
2017-05-14 12:26:25 +0700

    render forget password input

--- cageur-ui/src/routes/Login.js
+++ cageur-ui/src/routes/Login.js
@@ -21,6 +21,7 @@ export default class Login extends Component {
       password: '',
       error: false,
       errorText: '',
+      forgetPassword: false,
     };
   }
 
@@ -133,6 +134,34 @@ export default class Login extends Component {
     })
   }
 
+  handleForgetPassword(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if(this.state.email.length !== 0 && !validateEmail(this.state.email)) {
+      this.setState({
+        error: true,
+        errorText: 'Alamat email tidak valid'
+      })
+
+      return;
+    }
+    this.setState({
+        error: false,
+        forgetPassword: false,
+        errorText: ''
+    })
+  }
+
+  handleForgetPasswordClik(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    this.setState({
+        forgetPassword: true,
+    })
+  }
+  
   handleAlertDismiss() {
     this.setState({
       error: false,
@@ -145,6 +174,94 @@ export default class Login extends Component {
     (<Alert bsStyle="danger" onDismiss={::this.handleAlertDismiss}>
         <strong>{this.state.errorText} </strong>
     </Alert>) : "";
+
+    let renderSignIn = (this.state.forgetPassword) ?
+    (<PanelBody style={{padding: 0}}>
+      <div className='text-center bg-darkblue fg-white'>
+        <h3 style={{margin: 0, padding: 25}}>Lupa Password</h3>
+      </div>   
+      <div className='bg-hoverblue fg-black50 text-center' style={{padding: 12.5}}>  
+          <div>Masukan email untuk mereset password</div>
+          {renderError}
+          <Form onSubmit={::this.handleForgetPassword}>
+            <FormGroup>
+              <InputGroup bsSize='large'>
+                <InputGroup.Addon>
+                  <Icon glyph='icon-fontello-mail' />
+                </InputGroup.Addon>
+                <FormControl autoFocus
+                              type='email'
+                              className='border-focus-blue'
+                              placeholder='cageur@email.com'
+                              value={this.state.email}
+                              onChange={::this.handleEmailInput}
+                              />
+              </InputGroup>
+            </FormGroup>
+            <FormGroup>
+              <Grid>
+                <Row>
+                  <Col xs={6} collapseLeft collapseRight className='text-right'>
+                    <Button outlined lg type='submit' bsStyle='blue' onClick={::this.handleForgetPassword}>Kirim</Button>
+                  </Col>
+                </Row>
+              </Grid>
+            </FormGroup>
+          </Form>
+      </div>
+    </PanelBody>) :
+    (<PanelBody style={{padding: 0}}>
+      <div className='text-center bg-darkblue fg-white'>
+        <h3 style={{margin: 0, padding: 25}}>Masuk ke Cageur</h3>
+      </div>
+      <div className='bg-hoverblue fg-black50 text-center' style={{padding: 12.5}}>
+        <div>Anda perlu masuk ke Cageur terlebih dahulu</div>
+        {renderError}
+        <div style={{padding: 25, paddingTop: 0, paddingBottom: 0, margin: 'auto', marginBottom: 25, marginTop: 25}}>
+          <Form onSubmit={::this.handleLogin}>
+            <FormGroup controlId='emailaddress'>
+              <InputGroup bsSize='large'>
+                <InputGroup.Addon>
+                  <Icon glyph='icon-fontello-mail' />
+                </InputGroup.Addon>
+                <FormControl autoFocus
+                              type='email'
+                              className='border-focus-blue'
+                              placeholder='cageur@email.com'
+                              value={this.state.email}
+                              onChange={::this.handleEmailInput}
+                              />
+              </InputGroup>
+            </FormGroup>
+            <FormGroup controlId='password'>
+              <InputGroup bsSize='large'>
+                <InputGroup.Addon>
+                  <Icon glyph='icon-fontello-key' />
+                </InputGroup.Addon>
+                <FormControl type='password'
+                            className='border-focus-blue'
+                            placeholder='sandi'
+                            value={this.state.password}
+                            onChange={::this.handlePasswordInput}
+                            />
+              </InputGroup>
+            </FormGroup>
+            <FormGroup>
+              <Grid>
+                <Row>
+                  <Col xs={2} collapseLeft collapseRight style={{paddingTop: 10}}>
+                    <a onClick={::this.handleForgetPasswordClik} style={{cursor: 'pointer'}}>Lupa Sandi</a>
+                  </Col>
+                  <Col xs={6} collapseLeft collapseRight className='text-right'>
+                    <Button outlined lg type='submit' bsStyle='blue' onClick={::this.handleLogin}>Masuk</Button>
+                  </Col>
+                </Row>
+              </Grid>
+            </FormGroup>
+          </Form>
+        </div>
+      </div>
+    </PanelBody>);
     return (
       <div id='auth-container' className='login'>
         <div id='auth-row'>
@@ -154,55 +271,7 @@ export default class Login extends Component {
                 <Col sm={4} smOffset={4} xs={10} xsOffset={1} collapseLeft collapseRight>
                   <PanelContainer controls={false}>
                     <Panel>
-                      <PanelBody style={{padding: 0}}>
-                        <div className='text-center bg-darkblue fg-white'>
-                          <h3 style={{margin: 0, padding: 25}}>Masuk ke Cageur</h3>
-                        </div>
-                        <div className='bg-hoverblue fg-black50 text-center' style={{padding: 12.5}}>
-                          <div>Anda perlu masuk ke Cageur terlebih dahulu</div>
-                          {renderError}
-                          <div style={{padding: 25, paddingTop: 0, paddingBottom: 0, margin: 'auto', marginBottom: 25, marginTop: 25}}>
-                            <Form onSubmit={::this.handleLogin}>
-                              <FormGroup controlId='emailaddress'>
-                                <InputGroup bsSize='large'>
-                                  <InputGroup.Addon>
-                                    <Icon glyph='icon-fontello-mail' />
-                                  </InputGroup.Addon>
-                                  <FormControl autoFocus
-                                                type='email'
-                                                className='border-focus-blue'
-                                                placeholder='cageur@email.com'
-                                                value={this.state.email}
-                                                onChange={::this.handleEmailInput}
-                                                />
-                                </InputGroup>
-                              </FormGroup>
-                              <FormGroup controlId='password'>
-                                <InputGroup bsSize='large'>
-                                  <InputGroup.Addon>
-                                    <Icon glyph='icon-fontello-key' />
-                                  </InputGroup.Addon>
-                                  <FormControl type='password'
-                                              className='border-focus-blue'
-                                              placeholder='sandi'
-                                              value={this.state.password}
-                                              onChange={::this.handlePasswordInput}
-                                              />
-                                </InputGroup>
-                              </FormGroup>
-                              <FormGroup>
-                                <Grid>
-                                  <Row>
-                                    <Col xs={6} collapseLeft collapseRight className='text-right'>
-                                      <Button outlined lg type='submit' bsStyle='blue' onClick={::this.handleLogin}>Masuk</Button>
-                                    </Col>
-                                  </Row>
-                                </Grid>
-                              </FormGroup>
-                            </Form>
-                          </div>
-                        </div>
-                      </PanelBody>
+                      {renderSignIn}
                     </Panel>
                   </PanelContainer>
                 </Col>

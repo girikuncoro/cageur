a98c467ddfb3f8d7b8c4c4a65937056a64496685
Giri Kuncoro <gkuncoro@vmware.com>
2017-05-14 18:30:21 -0700

    SSL postgres for production

--- cageur-api/app/config/db.js
+++ cageur-api/app/config/db.js
@@ -1,6 +1,11 @@
 const promise = require('bluebird');
 const pgp = require('pg-promise')({ promiseLib: promise });
+let db;
 
-const db = pgp(require('../config').postgresUrl);
+if (process.env.NODE_ENV === 'production') {
+  db = pgp(require('../config').postgresUrl + '?ssl=true');
+} else {
+  db = pgp(require('../config').postgresUrl);
+}
 
 module.exports = db;
